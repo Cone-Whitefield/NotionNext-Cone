8dbc1defed921efbdcb82b471a3f2eed38a95ec4
Cone-Whitefield <34313152+Cone-Whitefield@users.noreply.github.com>
2026-08-14 13:16:33 +0000

    Update config.js

--- themes/matery/config.js
+++ themes/matery/config.js
@@ -29,14 +29,14 @@ const CONFIG = {
   MATERY_POST_LIST_PREVIEW: true, // 读取文章预览
 
   MATERY_ARTICLE_ADJACENT: false, // 显示上一篇下一篇文章推荐
-  MATERY_ARTICLE_COPYRIGHT: true, // 文章版权声明：true 全部显示；false 全部关闭；custom 仅填写 copyright 时显示
+  MATERY_ARTICLE_COPYRIGHT: false, // 文章版权声明：true 全部显示；false 全部关闭；custom 仅填写 copyright 时显示
   MATERY_ARTICLE_NOT_BY_AI: false, // 显示非AI写作
   MATERY_ARTICLE_RECOMMEND: false, // 文章关联推荐
 
   MATERY_WIDGET_LATEST_POSTS: true, // 显示最新文章卡
   MATERY_WIDGET_ANALYTICS: false, // 显示统计卡
   MATERY_WIDGET_TO_TOP: true,
-  MATERY_WIDGET_TO_COMMENT: true, // 跳到评论区
+  MATERY_WIDGET_TO_COMMENT: false, // 跳到评论区
   WIDGET_DARK_MODE: false, // 夜间模式
   MATERY_WIDGET_TOC: true, // 移动端悬浮目录
 
